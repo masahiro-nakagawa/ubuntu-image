ecd2d8f2409f54a6b903511a373ee3e441da3671
Paul Mars <paul.mars@canonical.com>
2023-12-14 17:04:00 +0100

    Improve user management customization

--- internal/imagedefinition/error.go
+++ internal/imagedefinition/error.go
@@ -4,4 +4,5 @@ import "errors"
 
 var (
 	ErrKeepEnabledNil = errors.New("KeepEnabled is nil. Thi value cannot be properly used.")
+	ErrExpireNil      = errors.New("Expire is nil. This value cannot be properly used.")
 )
